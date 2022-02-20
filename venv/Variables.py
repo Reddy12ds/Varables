29cba8f309821c62d806910130e7bc3df68602f7
Reddy12ds <reddy12ds@gmail.com>
2022-02-20 12:06:37 -0500

    New commit

---
+++ venv/Variables.py
@@ -0,0 +1,19 @@
+
+first_name = "Reddy"
+last_name = "K"
+full_name = first_name +" "+ last_name
+print("Hello "+full_name)
+print(type(full_name))
+
+age = 21
+age += 1
+print("your age is: "+str(age))
+print(type(age))
+
+height = 250.5
+print("Your height is: "+str(height)+"cm")
+print(type(height))
+
+human = False
+print("re you a human: "+str(human))
+print(type(human))
